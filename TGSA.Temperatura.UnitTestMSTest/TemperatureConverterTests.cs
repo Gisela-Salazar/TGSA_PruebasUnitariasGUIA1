9a3b7927be9535adbbe51033222f5e34096a5e9c
Gisela-Salazar <thanysalazar4@gmail.com>
2025-10-14 20:43:22 -0600

    TemperatureTest

--- TGSA.Temperatura.UnitTestMSTest/TemperatureConverterTests.cs
+++ TGSA.Temperatura.UnitTestMSTest/TemperatureConverterTests.cs
@@ -1,12 +1,26 @@
-﻿using System;
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TGSA.Temperatura;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace TGSA.Temperatura.UnitTestMSTest
+namespace TGSA.Temperatura.Tests
 {
-    internal class TemperatureConverterTests
+    [TestClass()]
+    public class TemperatureConverterTests
     {
+        [TestMethod()]
+        public void CelsiusToFahrenheitTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void FahrenheitToCelsiusTest()
+        {
+            Assert.Fail();
+        }
     }
 }
