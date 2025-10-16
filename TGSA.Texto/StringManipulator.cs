2fbdc8ee61b5ec8ee04c60157ef5e112fb6cda19
Gisela-Salazar <thanysalazar4@gmail.com>
2025-10-16 13:16:14 -0600

    Metodos a probar de la clase StringManipulator

--- TGSA.Texto/StringManipulator.cs
+++ TGSA.Texto/StringManipulator.cs
@@ -6,7 +6,22 @@ using System.Threading.Tasks;
 
 namespace TGSA.Texto
 {
-    internal class StringManipulator
+    public class StringManipulator
     {
+        // Invierte la cadena recibida 
+        public string ReverseString(string input)
+        {
+            if (input == null) return null;
+            char[] chars = input.ToCharArray();
+            System.Array.Reverse(chars);
+            return new string(chars);
+        }
+
+        // Elimina todos los espacios en blanco
+        public string RemoveSpaces(string input)
+        {
+            if (input == null) return null;
+            return input.Replace(" ", string.Empty);
+        }
     }
 }
